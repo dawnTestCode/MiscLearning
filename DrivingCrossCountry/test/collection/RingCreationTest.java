d7648a2e5c3e9e058e3d23e53f0e8a19e3e1b066
Patrick Wilson-Welsh <patrickwilsonwelsh@gmail.com>
2011-03-23 17:50:12 -0400

    changes to CrossCOuntryTrip

--- DrivingCrossCountry/test/collection/RingCreationTest.java
+++ DrivingCrossCountry/test/collection/RingCreationTest.java
@@ -1,5 +1,12 @@
 package collection;
 
+//Test List
+//TODO Can create new Ring
+//TODO Can add Stop to Ring
+//TODO Can retrieve total number of Stop in Ring
+//TODO Can traverse Ring fully
+//TODO Can remove Stop from Ring 
+
 
 public class RingCreationTest {
 
