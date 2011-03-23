1d68fc8febc3df2f7e754f4383213572e629cee8
Patrick Wilson-Welsh <patrickwilsonwelsh@gmail.com>
2011-03-23 18:09:06 -0400

    updating requirements

--- DrivingCrossCountry/test/collection/RingCreationTest.java
+++ DrivingCrossCountry/test/collection/RingCreationTest.java
@@ -3,8 +3,9 @@ package collection;
 //Test List
 //TODO Can create new Ring
 //TODO Can add Place to Ring
+//TODO Can add second Place to Ring
 //TODO Can retrieve total number of Place in Ring
-//TODO Can traverse Ring fully
+//TODO Can traverse Ring fully, asserting Place name as you go
 //TODO Can remove Place from Ring 
 
 
